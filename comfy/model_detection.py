f633f81089adab05c0af948f84e8addf87f97aff
josephrocca <1167575+josephrocca@users.noreply.github.com>
2025-07-08 23:15:25 +0800

    Set correct patch size for new Chroma VAE

--- comfy/model_detection.py
+++ comfy/model_detection.py
@@ -178,6 +178,8 @@ def detect_unet_config(state_dict, key_prefix, metadata=None):
         dit_config["qkv_bias"] = True
         if '{}distilled_guidance_layer.0.norms.0.scale'.format(key_prefix) in state_dict_keys or '{}distilled_guidance_layer.norms.0.scale'.format(key_prefix) in state_dict_keys: #Chroma
             dit_config["image_model"] = "chroma"
+            patch_size = 1
+            dit_config["patch_size"] = patch_size
             dit_config["in_channels"] = 64
             dit_config["out_channels"] = 64
             dit_config["in_dim"] = 64
